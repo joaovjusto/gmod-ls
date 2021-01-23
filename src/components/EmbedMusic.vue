4a1a366276b15edc41a626642e14d7e57de4d28c
dev-marinho <leonardo.msnt@outlook.com>
2021-01-23 01:44:55 -0300

    Adicionado componente EmbedMusic #3

---
+++ src/components/EmbedMusic.vue
@@ -0,0 +1,136 @@
+<template>
+  <div id="embed-music" class="tag">
+    <div id="player-wrap">
+      <audio id="player" :src="music.url" autoplay allow="autoplay" />
+    </div>
+    <div id="summary-wrap" class="animation-ease-1s">
+      <div id="thumb-wrap" class="summary-data-wrap">
+        <div class="summary-data">
+          <div id="thumb" :style="music.thumb" />
+        </div>
+      </div>
+      <div id="info-wrap" class="summary-data-wrap">
+        <div id="info" class="summary-data">
+          <span id="name">{{ music.title }}</span>
+          <span id="channel">{{ music.artist }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
+import Vue from 'vue';
+
+const Component = Vue.extend({
+  name: 'EmbedMusic',
+  props: {
+    musicsUrlList: {
+      type: Array,
+      default: () => [],
+      required: true,
+    },
+    volume: {
+      type: Number,
+      validator: val => val >= 0 && val <= 1,
+    },
+  },
+  data() {
+    return {
+      index: 0,
+      indexMax: this.$props.musicsUrlList.length,
+      music: {
+        artist: 'author',
+        title: 'name',
+        thumb: 'thumb',
+        url: '',
+      },
+      player: '',
+    };
+  },
+  methods: {
+    generateThumbStyle() {
+      this.music.thumb = `background: url(${this.music.thumb}`;
+    },
+    initAudio() {
+      this.player = document.getElementById('player');
+      this.player.addEventListener('ended', this.onEnding);
+      this.loadMusic();
+      console.log(`Thumb: ${this.music.thumb}`);
+    },
+    loadMusic() {
+      this.music = this.$props.musicsUrlList[this.index];
+      this.generateThumbStyle();
+      this.nextIndex();
+    },
+    nextIndex() {
+      this.index = this.index + 1 >= this.indexMax ? 0 : this.index + 1;
+    },
+    onEnding() {
+      this.loadMusic();
+    },
+  },
+  mounted() {
+    this.initAudio();
+  },
+});
+
+export default Component;
+</script>
+
+<style scoped>
+#embed-music {
+  width: 300px;
+  height: 70px;
+  padding: 0 5%;
+}
+
+#embed-music #player-wrap {
+  display: block;
+  position: absolute;
+  top: -300%;
+}
+
+#embed-music #summary-wrap {
+  width: 100%;
+  height: 100%;
+}
+
+#summary-wrap #thumb-wrap {
+  width: 20%;
+  float: left;
+}
+
+#summary-wrap #info-wrap {
+  width: 75%;
+  float: right;
+}
+
+#summary-wrap #thumb {
+  width: 45px;
+  height: 45px;
+  background-position: center !important;
+  background-size: cover !important;
+  border-radius: 100%;
+}
+
+#summary-wrap #info {
+  padding-left: 5%;
+}
+
+#summary-wrap #info span {
+  line-height: 110%;
+  display: block;
+}
+
+.summary-data-wrap {
+  height: 100%;
+  display: table;
+}
+
+.summary-data-wrap .summary-data {
+  display: table-cell;
+  vertical-align: middle;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,24 +2,15 @@
   <div class="home">
     <div class="custom d-flex flex-column align-items-start justify-content-between">
       <div id="server-widget" class="p-5 position-top">
-        <span id="server-name" class="d-block">{{ bindServerInfo.serverName }}</span>
+        <span id="server-name" class="d-block">{{ bindServerData.name }}</span>
         <div class="d-block">
-          <span id="server-slots" class="tag">{{ bindApiInfo.slots }} players</span>
-          <span id="server-gamemode" class="tag">{{ bindApiInfo.gamemode }}</span>
-          <span id="server-map" class="tag">{{ bindApiInfo.map }}</span>
+          <span id="server-slots" class="tag">{{ bindServerData.slots }} players</span>
+          <span id="server-gamemode" class="tag">{{ bindServerData.gamemode }}</span>
+          <span id="server-map" class="tag">{{ bindServerData.map }}</span>
         </div>
       </div>
       <div class="p-5 position-bottom">
-        <div id="music-widget" class="tag">
-          <div id="music-img">
-            <div id="music-img-container">
-              <img src="@/assets/music.png" />
-            </div>
-          </div>
-          <div id="music-name">
-            <span>Queen - Show Must Go On</span>
-          </div>
-        </div>
+        <EmbedMusic :musicsUrlList="bindServerData.musicsUrlList" :apiKey="bindServerData.apiKey" />
       </div>
     </div>
   </div>
@@ -28,77 +19,46 @@
 <script lang="ts">
 /* eslint-disable @typescript-eslint/ban-ts-ignore */
 import Vue from 'vue';
+import { EmbedMusic } from '@/components';
 // @ts-ignore
-import serverInfo from '@/assets/serverInfo.json';
+import serverData from '@/assets/serverInfo.json';
 // @ts-ignore
 import apiData from '@/assets/demoapidata.json';
 
 export default Vue.extend({
   name: 'Home',
+  components: {
+    EmbedMusic,
+  },
   data() {
     return {
-      bindApiInfo: apiData,
-      bindServerInfo: serverInfo,
+      bindServerData: serverData,
+      bindServerInfo: apiData,
     };
+  },
+  methods: {
+    GameDetails(
+      servername: any,
+      serverurl: any,
+      mapname: any,
+      maxplayers: any,
+      steamid: any,
+      gamemode: any,
+      volume: any,
+      language: any,
+    ) {
+      this.bindServerData = {
+        gamemode,
+        map: mapname,
+        name: servername,
+        slots: maxplayers,
+      };
+    },
   },
 });
 </script>
 
 <style lang="scss" scoped>
-.position-top {
-  position: absolute;
-  top: 0;
-  left: 0;
-}
-.position-bottom {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-}
-
-.tag {
-  background: #fff;
-  border-radius: 4px;
-  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.5);
-  padding: 1% 2%;
-}
-
-#music-widget {
-  width: 300px;
-  height: 70px;
-  padding: 0 5%;
-}
-
-#music-img,
-#music-name {
-  height: 100%;
-  display: table;
-  float: left;
-}
-
-#music-img {
-  margin-right: 30px;
-}
-
-#music-img #music-img-container,
-#music-name span {
-  display: table-cell;
-  vertical-align: middle;
-}
-
-#music-img #music-img-container img {
-  width: 40px;
-  height: 40px;
-  border-radius: 100%;
-}
-
-#music-name span {
-  max-width: 170px;
-  text-align: center;
-  text-transform: uppercase;
-  overflow: hidden;
-}
-
 #server-widget span {
   text-transform: uppercase;
 }
@@ -118,5 +78,15 @@ export default Vue.extend({
   font-weight: 400;
   text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.1);
   margin-right: 3%;
+}
+.position-top {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.position-bottom {
+  position: absolute;
+  bottom: 0;
+  left: 0;
 }
 </style>
