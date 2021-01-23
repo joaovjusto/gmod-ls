ddcc22e7830caa1760c6e971a870de265713a2b2
dev-marinho <leonardo.msnt@outlook.com>
2021-01-23 01:47:44 -0300

    Adicionado componente Carrousel ao index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import BackgroundGradient from '@/components/BackgroundGradient.vue';
+import Carrousel from '@/components/Carrousel.vue';
 import EmbedMusic from '@/components/EmbedMusic.vue';
 
-export { BackgroundGradient, EmbedMusic };
+export { BackgroundGradient, Carrousel, EmbedMusic };
