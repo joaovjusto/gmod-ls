41528186bee18f935abf95e399139329ad78e796
dev-marinho <leonardo.msnt@outlook.com>
2021-01-26 06:22:16 -0300

    Adicionada camada média do gradiente

--- src/components/BackgroundGradient.vue
+++ src/components/BackgroundGradient.vue
@@ -2,9 +2,9 @@
   <div class="bg-gradient">
     <svg xmlns="http://www.w3.org/2000/svg">
       <defs>
-        <linearGradient :id="hash" x1="0%" y1="50%" x2="100%" y2="50%">
-          <stop offset="0%" :style="styleStart" />
+        <linearGradient :id="hash" x1="0%" y1="0%" x2="100%" y2="0%">
           <stop :offset="offsetStart" :style="styleStart" />
+          <stop :offset="offsetMiddle" :style="styleMiddle" />
           <stop offset="100%" :style="styleEnd" />
         </linearGradient>
       </defs>
@@ -49,7 +49,9 @@ const Component = Vue.extend({
     return {
       hash: '',
       offsetStart: '',
+      offsetMiddle: '',
       styleEnd: '',
+      styleMiddle: '',
       styleStart: '',
       url: '',
     };
@@ -57,7 +59,9 @@ const Component = Vue.extend({
   methods: {
     initData() {
       this.offsetStart = `${this.$props.ratio * 100}%`;
+      this.offsetMiddle = `${this.$props.ratio * 1.75 * 100}%`;
       this.styleEnd = `stop-color:${this.$props.colorEnd};stop-opacity:${this.$props.opacityEnd}`;
+      this.styleMiddle = `stop-color:${this.$props.colorStart};stop-opacity:${this.$props.opacityStart * 0.5}`;
       this.styleStart = `stop-color:${this.$props.colorStart};stop-opacity:${this.$props.opacityStart}`;
       this.hash = `grad${document.getElementsByClassName('bg-gradient').length}`;
       this.url = `url(#${this.hash})`;
@@ -76,6 +80,7 @@ export default Component;
   width: 100%;
   height: 100%;
   background: transparent;
+  opacity: 0.1;
   position: absolute;
   top: 0;
   left: 0;
